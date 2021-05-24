2496960f610a165c308a1ae0d2d20b198afd74a9
Ben Lao <benlau413@gmail.com>
2021-05-25 00:25:53 +0800

    change blockchain transaction include lat, long and description

--- app/blockchain.py
+++ app/blockchain.py
@@ -37,18 +37,12 @@ class Blockchain(object):
         self.chain.append(block)
         return block
 
-    def new_transaction(self, sender, recipient, amount):
-        """
-        Creates a new transaction to go into the next mined Block
-        :param sender: <str> Address of the Sender
-        :param recipient: <str> Address of the Recipient
-        :param amount: <int> Amount
-        :return: <int> The index of the Block that will hold this transaction
-        """
+    def new_transaction(self, longitude, latitude, description):
+
         self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
+            'longitude': longitude,
+            'latitude': latitude,
+            'description': description,
         })
 
         return self.last_block['index'] + 1
