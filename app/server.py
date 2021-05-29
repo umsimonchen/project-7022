8110398131040f6f2abf3940e371ebca8f470549
Ben Lao <benlau413@gmail.com>
2021-05-30 02:38:48 +0800

    finish get_coordinates api

--- app/server.py
+++ app/server.py
@@ -5,6 +5,8 @@
 from uuid import uuid4
 from flask import Flask, jsonify, request, render_template
 from blockchain import Blockchain
+from map_data import Coordinate
+import traceback
 import numpy as np
 import geopandas
 import json
@@ -16,11 +18,13 @@ from rtree import index
 app = Flask(__name__, template_folder='../templates', static_folder = '../static')
 
 # Generate a globally unique address for this node
-k=1000 #1000 users
+k=1000 #1000 usersl
 node_identifiers = [str(uuid4()).replace('-', '') for i in range(k)]
 # Instantiate the Blockchain
 blockchain = Blockchain()
+coordinate = Coordinate()
 node_index=0
+amenity_group = None
 
 world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
 
@@ -88,6 +92,34 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     return jsonify(response), 200
+
+## get coordinate data api
+@app.route('/get_coordinates', methods=['GET'])
+def get_coordinates():
+    input = {}
+    for i in ["lat_min", "lng_min", "lat_max", "lng_max", "type_name"]:
+        value = request.args.get(i)
+        if i in ["lat_min", "lng_min", "lat_max", "lng_max"]:
+            try:
+                if value is None or value == "": return "Key '%s' must be filled in." % i, 400     ## check all coordinates params has been filled in
+                value = float(value)
+            except ValueError as e:
+                return "The value of key '%s' must be float." % i, 400
+        input[i] = value
+    try:
+        amenity_group = coordinate.get_amenity_from_osm(input["lat_min"], input["lng_min"], input["lat_max"], input["lng_max"])
+    except Exception as e:
+        return error_handling(e)
+    response = {
+        "get_coordinates_number": len(amenity_group),
+        "info": amenity_group.to_dict('index')
+    }
+    return jsonify(response), 200
+
+## error handle and show traceback
+def error_handling(e):
+    traceback.print_exc()
+    return ("%s: %s" % (type(e).__name__, e.args[0]), 400)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
