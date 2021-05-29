c2f5299859cf5468eebeefafd2cafcaa1b36733b
Ben Lao <benlau413@gmail.com>
2021-05-30 02:38:28 +0800

    map_data.py change tag to type_name

--- app/map_data.py
+++ app/map_data.py
@@ -2,9 +2,9 @@ from pandana.loaders import osm
 
 class Coordinate():
     # tag can choose: [restaurant, cafe, bar, school, university]
-    def get_amenity_from_osm(self, lat_min, lng_min, lat_max, lng_max, tag):
+    def get_amenity_from_osm(self, lat_min, lng_min, lat_max, lng_max, type_name='restaurant'):
         amenity_group = osm.node_query(
-            lat_min, lng_min, lat_max, lng_max, tags='"amenity"="#{tag}"')
+            lat_min, lng_min, lat_max, lng_max, tags='"amenity"="%s"' % type_name)
         return amenity_group[["lat", "lon", "name"]]
 
     def get_one_sample_from_amenity(self, amenity_group):
