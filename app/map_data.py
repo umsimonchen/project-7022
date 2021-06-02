c70660967084391a3e94dec16640dca49ef0d8d0
Ben Lao <benlau413@gmail.com>
2021-06-02 10:44:30 +0800

    change lng to lon, rtree update add action in input

--- app/map_data.py
+++ app/map_data.py
@@ -11,16 +11,16 @@ class Coordinate():
     return self.amenity_group == None
 
 # tag can choose: [restaurant, cafe, bar, school, university]
-  def get_amenity_from_osm(self, lat_min, lng_min, lat_max, lng_max, type_name='restaurant'):
+  def get_amenity_from_osm(self, lat_min, lon_min, lat_max, lon_max, type_name='restaurant'):
     amenity_group_total = osm.node_query(
-      lat_min, lng_min, lat_max, lng_max, tags='"amenity"="%s"' % type_name)
-    self.amenity_group = amenity_group_total[["lat", "lng", "name"]]
+      lat_min, lon_min, lat_max, lon_max, tags='"amenity"="%s"' % type_name)
+    self.amenity_group = amenity_group_total[["lat", "lon", "name"]]
     return self.amenity_group
 
   def get_one_sample_from_amenity(self, amenity_group):
     sample = self.amenity_group.sample(1)
     result = {}
-    for i in ["index", "lat", "lng", "name"]:
+    for i in ["index", "lat", "lon", "name"]:
       value = getattr(sample, i).values[0]  # same as sample.#{key_name}.values[0]
       result[i] = value
     return result
--- app/rtree_update.py
+++ app/rtree_update.py
@@ -8,14 +8,14 @@ class Rtree():
   def get_rtree_index(self):
     return self.block_count, self.idx
 
-  def update_index(self, point):    ## point: {id, lat, lng, name, action: (insert=0/delete=1)}
-    if point["action"] == 0:
-      self.idx.insert(point["id"], (point["lat"], point["lng"], point["lat"], point["lng"]), obj = point["name"])
+  def update_index(self, point, action):    ## point: {id, lat, lon, name, action: (insert=0/delete=1)}
+    if action == 0:
+      self.idx.insert(point["id"], (point["lat"], point["lon"], point["lat"], point["lon"]), obj = point["name"])
       return "Insert Success"
-    elif point["action"] == 1:
-      self.idx.delete(point["id"], (point["lat"], point["lng"], point["lat"], point["lng"]))
+    elif action == 1:
+      self.idx.delete(point["id"], (point["lat"], point["lon"], point["lat"], point["lon"]))
       return "Delete Success"
 
-  def get_nearest_k_point(self, input_loc, k):    ## input loc: {lat, lng}
-    hits = self.idx.nearest((input_loc["lat"], input_loc["lng"], input_loc["lat"], input_loc["lng"]), object = True)
+  def get_nearest_k_point(self, input_loc, k):    ## input loc: {lat, lon}
+    hits = self.idx.nearest((input_loc["lat"], input_loc["lon"], input_loc["lat"], input_loc["lon"]), k, objects = True)
     return hits
