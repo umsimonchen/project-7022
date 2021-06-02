64bfd1390031ac05a32b212c966ff72352214b9a
Ben Lao <benlau413@gmail.com>
2021-06-02 10:37:35 +0800

    update lon to lng

--- app/map_data.py
+++ app/map_data.py
@@ -14,13 +14,13 @@ class Coordinate():
   def get_amenity_from_osm(self, lat_min, lng_min, lat_max, lng_max, type_name='restaurant'):
     amenity_group_total = osm.node_query(
       lat_min, lng_min, lat_max, lng_max, tags='"amenity"="%s"' % type_name)
-    self.amenity_group = amenity_group_total[["lat", "lon", "name"]]
+    self.amenity_group = amenity_group_total[["lat", "lng", "name"]]
     return self.amenity_group
 
   def get_one_sample_from_amenity(self, amenity_group):
     sample = self.amenity_group.sample(1)
     result = {}
-    for i in ["index", "lat", "lon", "name"]:
+    for i in ["index", "lat", "lng", "name"]:
       value = getattr(sample, i).values[0]  # same as sample.#{key_name}.values[0]
       result[i] = value
     return result
